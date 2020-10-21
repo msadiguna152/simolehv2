bf8c5a2005d2660d063a9ed06a049c156d9dd807
Danar Widi <widiu7omo@gmail.com>
2020-10-22 01:42:57 +0800

    - Text Button - diganti jadi "Tambah" dgn posisi diturunkan (tdk sejajar dgn harga) - Set QTY lgsng stlh hit button Tambah & oto kalkulasi di Cart - Saat QTY 0 Button - dan + berubah jadi Tambah lg - Selama belanja qty tetap nilainya pd product

--- application/views/beranda-js.php
+++ application/views/beranda-js.php
@@ -2,11 +2,8 @@
 	const berandaHelper = {
 		showToast(toast) {
 			Snackbar.show({text: toast.message});
-		}
-	}
-	$(document).ready(function () {
-		$(document).on('click', '#btn-add-cart', function () {
-			var id = $(this).data('id');
+		},
+		tambahQty(id) {
 			var keranjang = localStorage.getItem('keranjang');
 			if (keranjang !== null) {
 				var decodeKeranjang = JSON.parse(keranjang);
@@ -21,6 +18,55 @@
 				localStorage.setItem('keranjang', JSON.stringify(itemBaru));
 			}
 			berandaHelper.showToast({message: 'Berhasil masuk dalam keranjang'})
+		}, kurangQty(id) {
+			var keranjang = localStorage.getItem('keranjang');
+			if (keranjang !== null) {
+				var decodeKeranjang = JSON.parse(keranjang);
+				var isExist = id in decodeKeranjang;
+				if (isExist) {
+					if (decodeKeranjang[id].count > 1) {
+						decodeKeranjang[id] = isExist ? {count: decodeKeranjang[id].count - 1} : {count: 1};
+					} else {
+						delete decodeKeranjang[id];
+					}
+				}
+				var jumlahIsi = Object.keys(decodeKeranjang);
+				$('#cart-item-count').text(jumlahIsi.length);
+				localStorage.setItem('keranjang', JSON.stringify(decodeKeranjang));
+			} else {
+				var itemBaru = {};
+				itemBaru[id] = {count: 1};
+				localStorage.setItem('keranjang', JSON.stringify(itemBaru));
+			}
+			berandaHelper.showToast({message: 'Berhasil mengurangi item keranjang'})
+		}
+	}
+	$(document).ready(function () {
+		$(document).on('click', '#btn-add-cart', function () {
+			$(this).hide();
+			$(this).siblings('div.cart-items-number').show();
+			var id = $(this).data('id');
+			berandaHelper.tambahQty(id);
+			return false;
+		}).on('click', '#button-main-plus', function () {
+			var inputQty = $(this).parent('div').siblings('input');
+			inputQty.val(parseInt(inputQty.val()) + 1);
+			berandaHelper.tambahQty($(this).data('id'));
+			return false;
+		}).on('click', '#button-main-minus', function () {
+			var inputQty = $(this).parent('div').siblings('input');
+			var currentVal = parseInt(inputQty.val());
+			if (currentVal > 1) {
+				inputQty.val(currentVal - 1);
+				berandaHelper.kurangQty($(this).data('id'));
+			} else {
+				var containerButton = $(this).parents('div#container-button');
+				containerButton.find('#btn-add-cart').show();
+				$(this).parents('div#container-qty-btn').hide();
+				berandaHelper.kurangQty($(this).data('id'));
+				inputQty.val(1);
+			}
+			return false;
 		})
 	})
 </script>
--- application/views/kategori.php
+++ application/views/kategori.php
@@ -6,20 +6,20 @@
 		</div>
 	</div>
 	<div class="px-3 bg-white pb-2">
-        <div class="pt-0">
-            <h2 class="font-weight-bold"><?= ucwords(str_replace("-", " ", htmlspecialchars($nama_kategori))); ?></h2>
-        </div>
-    </div>
+		<div class="pt-0">
+			<h2 class="font-weight-bold"><?= ucwords(str_replace("-", " ", htmlspecialchars($nama_kategori))); ?></h2>
+		</div>
+	</div>
 	<div class="osahan-listing px-3 bg-white">
 		<div class="row">
-			<?php 
+			<?php
 			foreach ($data_perkategori->result() as $data):
-                $nama_produk = strtolower(str_replace(" ", "-", "$data->nama_produk"));
+				$nama_produk = strtolower(str_replace(" ", "-", "$data->nama_produk"));
 
-			?>
+				?>
 				<div class="col-6 p-0 border-right border-bottom border-top">
 					<div class="list-card-image">
-						<a href="<?= base_url()?>beranda/detail_produk/<?= $nama_produk; ?>" class="text-dark">
+						<a href="<?= base_url() ?>beranda/detail_produk/<?= $nama_produk; ?>" class="text-dark">
 							<div class="member-plan position-absolute"><span class="badge m-3 badge-danger">10%</span>
 							</div>
 							<div class="p-3">
@@ -27,12 +27,26 @@
 									 class="img-fluid item-img w-100 mb-3">
 								<h6><?= htmlspecialchars($data->nama_produk); ?></h6>
 								<p><?= htmlspecialchars($data->deskripsi); ?></p>
-								<div class="d-flex align-items-center">
+								<div class="" id="container-button">
 									<h6 class="price m-0 text-success"><?= "Rp " . number_format(htmlspecialchars($data->harga), 0, ',', '.'); ?></h6>
-									<a href="javascript:void(0)" data-id="<?= $data->id_produk ?>"
-									   id="btn-add-cart"
-									   class="btn btn-success btn-sm ml-auto">+
-									</a>
+									<button data-id="<?= $data->id_produk ?>"
+											id="btn-add-cart"
+											class="btn btn-success btn-block ml-auto mt-2">Tambah
+									</button>
+									<div id="container-qty-btn" class="input-group input-spinner cart-items-number mx-auto mt-2"
+										 style="display: none">
+										<div class="input-group-prepend">
+											<button data-id="<?= $data->id_produk ?>" class="btn btn-success btn-sm"
+													type="button" id="button-main-plus"> +
+											</button>
+										</div>
+										<input type="text" class="form-control" value="1">
+										<div class="input-group-append">
+											<button data-id="<?= $data->id_produk ?>" class="btn btn-success btn-sm"
+													type="button" id="button-main-minus"> −
+											</button>
+										</div>
+									</div>
 								</div>
 							</div>
 						</a>
